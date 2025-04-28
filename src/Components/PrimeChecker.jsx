7827af1ac876cc7bb30ba24767109f99393554ad
Mamta Kumawat <mamtakumawaton@gmail.com>
2025-04-28 14:00:38 +0530

    js functionality added

--- src/Components/PrimeChecker.jsx
+++ src/Components/PrimeChecker.jsx
@@ -43,3 +43,19 @@ const PrimeChecker = () => {
 };
 
 export default PrimeChecker;
+
+/*---------------------Javascript code---------------------------*/
+
+// function isPrime(num){
+//   if (num<=1) return false
+//   if (num==2) return true
+
+//   for(let i=2 ; i<=num/2; i++){
+//     if (num%i==0){
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// console.log(isPrime(22))
